05708b2a4ce115f4b2b87e0e79a8e29209427855
Ankit Bisht <ankitbisht0301@gmail.com>
2024-03-15 00:18:22 +0530

    Updated CSS from : https://github.com/hiteshchoudhary/chai-aur-react/blob/main/05passwordGenerator/src/App.jsx

--- 05-PasswordGenerator/src/App.jsx
+++ 05-PasswordGenerator/src/App.jsx
@@ -35,43 +35,55 @@ function App() {
 
 	return (
 		<>
-			<div className="bg-slate-500">
-				<h1 className="text-center text-4xl text-white">Password Generator</h1>
-				<div>
+			<div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
+				<h1 className="text-white text-center my-3">Password Generator</h1>
+				<div className="flex shadow rounded-lg overflow-hidden mb-4">
 					<input
 						type="text"
-						placeholder="password"
+						placeholder="Password"
+						className="outline-none w-full py-1 px-3"
 						value={password}
 						readOnly
 						ref={passwordRef}
-					></input>
-					<button onClick={copyPasswordToClipboard}> Copy </button>
+					/>
+					<button
+						onClick={copyPasswordToClipboard}
+						className="outline-none bg-blue-700 text-white px-3 py-0.5"
+					>
+						Copy
+					</button>
 				</div>
-				<div>
-					<input
-						type="range"
-						min={8}
-						max={20}
-						value={length}
-						onChange={(e) => setLength(e.target.value)}
-					/>
-					<label className="cursor-pointer">Length : {length}</label>
-					<input
-						type="checkbox"
-						defaultChecked={includeNumber}
-						onChange={() => {
-							setIncludeNumber((value) => !value);
-						}}
-					/>
-					<label className="bg-white">Numbers</label>
-					<input
-						type="checkbox"
-						defaultChecked={includeSpecialChar}
-						onChange={() => {
-							setIncludeSpecialChar((value) => !value);
-						}}
-					/>
-					<label className="bg-white">Characters</label>
+				<div className="flex text-sm gap-x-2">
+					<div className="flex items-center gap-x-1">
+						<input
+							type="range"
+							min={8}
+							max={20}
+							value={length}
+							onChange={(e) => setLength(e.target.value)}
+						/>
+						<label className="cursor-pointer">Length : {length}</label>
+					</div>
+					<div className="flex items-center gap-x-1">
+						<input
+							type="checkbox"
+							defaultChecked={includeNumber}
+							onChange={() => {
+								setIncludeNumber((value) => !value);
+							}}
+						/>
+						<label htmlFor="numberInput">Numbers</label>
+					</div>
+					<div className="flex items-center gap-x-1">
+						<input
+							type="checkbox"
+							defaultChecked={includeSpecialChar}
+							onChange={() => {
+								setIncludeSpecialChar((value) => !value);
+							}}
+						/>
+						<label htmlFor="characterInput">Characters</label>
+					</div>
 				</div>
 			</div>
 		</>
